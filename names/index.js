b346b8016ad01393384dbf235894764d5c582a88
mahendra <mahendranaidu.222@gmail.com>
2023-08-26 07:23:35 +0000

    first name of people using 3 modules

---
+++ names/index.js
@@ -0,0 +1,7 @@
+const peopleName = require("../country/state/city/index");
+const getFirstNames = require("../utilities/utils/index");
+const getPeopleInCity = (peopleName) => {
+  return getFirstNames(peopleName);
+};
+
+module.exports = getPeopleInCity;
